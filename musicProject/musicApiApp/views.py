7c449afa21275b975ae25cd6b977d51afaa698ac
ananyakhanna <akkhanna2602@gmail.com>
2023-04-11 22:25:23 +0530

    second commit

--- musicProject/musicApiApp/views.py
+++ musicProject/musicApiApp/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
-
+import requests
 # Create your views here.
+
+def home(request):
+    return render(request, 'index.html')
+
+def search_results(request):
+    results = {}
+    search_term = request.GET.get('term')
+    if search_term:
+        url = f'https://itunes.apple.com/search?term={search_term}&media=music&entity=album'
+        response = requests.get(url)
+        data = response.json()
+        # print(data)
+        results = data['results']
+
+    context = {
+        'results': results
+    }
+    return render(request, 'search_results.html', context)
